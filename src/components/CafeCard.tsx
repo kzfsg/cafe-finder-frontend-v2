582edde87b4f065ad97c7f5daaeb9e1c5287702d
kzfsg <131367782+kzfsg@users.noreply.github.com>
2025-05-19 13:44:15 +0800

    power icon

--- src/components/CafeCard.tsx
+++ src/components/CafeCard.tsx
@@ -12,26 +12,28 @@ export default function CafeCard({ title, image, description, hasWifi = false, h
     <div className="cafe-card">
       <img src={image} alt={title} className="cafe-image" />
       <div className="cafe-content">
-        <h3 className="cafe-title">{title}</h3>
-        <p className="cafe-description">{description}</p>
-        <div className="cafe-card-footer">
-          <div className="cafe-amenities">
-            {hasWifi && (
-              <div className="amenity wifi" title="WiFi Available">
-                <img src="/icons/wifi.svg" alt="WiFi" />
-              </div>
-            )}
-            {hasPower && (
-              <div className="amenity power" title="Power Outlets Available">
-                <img src="/icons/power.svg" alt="Power Outlets" />
-              </div>
-            )}
-          </div>
-          <div className="upvotes-container" title="Upvotes">
-            <img src="/icons/upvote.svg" alt="Upvote" className="upvote-icon" />
-            <span className="upvotes-count">{upvotes}</span>
+        <div className="cafe-header">
+          <h3 className="cafe-title">{title}</h3>
+          <div className="cafe-meta">
+            <div className="cafe-amenities">
+              {hasWifi && (
+                <div className="amenity wifi" title="WiFi Available">
+                  <img src="/icons/wifi.svg" alt="WiFi" />
+                </div>
+              )}
+              {hasPower && (
+                <div className="amenity power" title="Power Outlets Available">
+                  <img src="/icons/power.svg" alt="Power Outlets" />
+                </div>
+              )}
+            </div>
+            <div className="upvotes-container" title="Upvotes">
+              <img src="/icons/upvote.svg" alt="Upvote" className="upvote-icon" />
+              <span className="upvotes-count">{upvotes}</span>
+            </div>
           </div>
         </div>
+        <p className="cafe-description">{description}</p>
       </div>
     </div>
   );
