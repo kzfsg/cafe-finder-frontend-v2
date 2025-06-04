30c26669f47c7bbc1c93eb7ef26970c5d1afa348
kzfsg <131367782+kzfsg@users.noreply.github.com>
2025-06-05 00:57:12 +0800

    test 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// Workaround for useLayoutEffect issue in React 19
+import React from 'react';
+React.useLayoutEffect = React.useEffect;
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 import { ErrorBoundary } from './components/ErrorBoundary';
 
 // Add error handler for uncaught errors
